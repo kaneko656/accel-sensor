b72098114e34b19d82ced4e8b5a85f053fa1a03c
kaneko656 <nakano656sho@gmail.com>
2017-02-01 22:15:17 +0900

    remove server

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ function signed(n) {
     return (n < 128) ? n : n - 256
 }
 
-function app_i2c(server) {
+function app_i2c() {
     console.log('app_i2c')
-    let io = require('socket.io')(server)
+    //let io = require('socket.io')(server)
     wire.writeBytes(0x16, [0x05], function(err, res) {})
     wire.writeBytes(0x10, [0, 0, 0, 0, 0, 0], function(err, res) {})
     setInterval(function() {
