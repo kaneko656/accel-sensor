7da7f1b438d95238720b2b6d0c2b40d496b4b043
kaneko656 <nakano656sho@gmail.com>
2017-02-02 16:42:41 +0900

    writebyte

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let router = express.Router()
 let i2c = require('i2c')
-let addr = 0x18
+let addr = 0x18 // i2cを選択するアドレス
 let wire = new i2c(addr, {
     device: '/dev/i2c-1'
 })
@@ -14,25 +14,25 @@ function signed(n) {
 function app_i2c() {
     console.log('app_i2c')
     //let io = require('socket.io')(server)
-    wire.writeBytes(0x16, [0x05], function(err, res) {
-        console.log('write')
+
+    // i2cを選択するアドレス
+    // センサごとの中のアドレス
+
+    wire.writeBytes(0x20, [0x7f], function(err, res) {
+        console.log('パワーダウンをオフ')
         console.log(err)
         console.log(res)
 
     })
-    wire.writeBytes(0x10, [0, 0, 0, 0, 0, 0], function(err, res) {
-      console.log('write2')
-      console.log(err)
-      console.log(res)
-
-    })
     setInterval(function() {
-        wire.readBytes(0x06, 3, function(err, res) {
+        wire.readBytes(0x28, 6, function(err, res) {
             console.log(err)
-            console.log(err)
-            let x = signed(res[0])
-            let y = signed(res[1])
-            let z = signed(res[2])
+            console.log(res)
+            // lower
+            // higher
+            let x = res[0] + signed(res[1]) * 256
+            let y = res[2] + signed(res[3]) * 256
+            let z = res[4] + signed(res[5]) * 256
             // io.sockets.emit('event', {
             //     x: x,
             //     y: y,
